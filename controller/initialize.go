0d916322fbdde526b3de17ae93edd7804e2df5c9
By Lin <eleven_g11@qq.com>
2022-02-03 23:43:02 +0800

    .

--- controller/initialize.go
+++ controller/initialize.go
@@ -2,15 +2,18 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"upv.life/server/middleware"
 )
 
 func Initialize(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		auth, _ := c.Get(middleware.GinCtxAuthKey)
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-			"auth":    auth,
+	api := r.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			// auth, _ := c.Get(middleware.GinCtxAuthKey)
+			c.JSON(200, gin.H{
+				"message": "Hello World",
+				// "auth":    auth.(middleware.AuthClaims),
+			})
 		})
-	})
+	}
+
 }
