3ec7b869386169cbaafaa38d65b16a94c9e21bc3
By Lin <eleven_g11@qq.com>
2022-02-05 16:16:19 +0800

    .

--- config/initialize.go
+++ config/initialize.go
@@ -8,11 +8,10 @@ import (
 )
 
 var (
-	AppMode          string
-	AppPort          string
-	MysqlDsn         string
-	JwtSalt          string
-	UserPasswordSalt string
+	AppMode  string
+	AppPort  string
+	MysqlDsn string
+	JwtSalt  string
 )
 
 func Initialize() {
@@ -23,5 +22,4 @@ func Initialize() {
 	AppPort = os.Getenv("APP_PORT")
 	MysqlDsn = os.Getenv("MYSQL_DSN")
 	JwtSalt = os.Getenv("JWT_SALT")
-	UserPasswordSalt = os.Getenv("USER_PASSWORD_SALT")
 }
