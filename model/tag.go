9059d1ee00c53a81bb40cd8b6974fe72b41e1f45
CY <shiyiya.11@gmail.com>
2022-05-12 15:29:06 +0800

    Update tag.go

--- model/tag.go
+++ model/tag.go
@@ -1,13 +1,8 @@
 package model
 
-import (
-	"gorm.io/gorm"
-)
-
 type Tag struct {
-	ID        uint           `gorm:"primaryKey"`
-	Uid       uint           `json:"-"`
-	Name      string         `gorm:"size:20"`
-	Synopsis  string         `gorm:"size:200"`
-	DeletedAt gorm.DeletedAt `json:"-"`
+	ID       uint   `gorm:"primaryKey"`
+	Uid      uint   `json:"-"`
+	Name     string `gorm:"size:20"`
+	Synopsis string `gorm:"size:200"`
 }
